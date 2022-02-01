eea52a9b910eaf52fb7122d96e10b3dd40fc266c
Sean Hungerford <seanhungerford@Seans-MacBook-Pro.local>
2022-01-31 18:54:08 -0600

    First commit Message

---
+++ Question1/src/mypack/SearchQuery.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class SearchQuery {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+		
+		
+	}
+
+}
